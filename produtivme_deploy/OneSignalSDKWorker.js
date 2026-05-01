9a7cbab57eed0e0e3b74d402b73268869965c5c8
wendellcww-dot <wendellcww@gmail.com>
2026-04-30 21:09:09 -0300

    Delete produtivme_deploy/OneSignalSDKWorker.js

--- produtivme_deploy/OneSignalSDKWorker.js
+++
@@ -1,4 +0,0 @@
-// OneSignalSDKWorker.js
-// Este arquivo precisa existir na raiz do projeto.
-// Ele redireciona para o sw.js principal que já importa o OneSignal internamente.
-importScripts('/sw.js');
