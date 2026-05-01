208e3616ffe4148a1e5de2d75604aa550c076554
wendellcww-dot <wendellcww@gmail.com>
2026-04-30 21:11:51 -0300

    Add files via upload

---
+++ produtivme_deploy/OneSignalSDKWorker.js
@@ -0,0 +1,4 @@
+// OneSignalSDKWorker.js
+// Importa diretamente o SDK do OneSignal.
+// NÃO importa o sw.js para evitar ciclo duplo de install.
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
